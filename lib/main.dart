161726243b210e2e7cdd58a57ec00b0ccb16ad81
TM Revanza <revanzanrndra@gmail.com>
2022-11-17 12:25:58 +0700

    Tutorial 8 Kelar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:fltr/page/to_do_page.dart';
 import 'package:flutter/material.dart';
-import 'package:fltr/form.dart';
+import 'package:fltr/page/form.dart';
 
 void main() {
   runApp(const MyApp());
@@ -95,6 +96,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
--- lib/page/form.dart
+++ lib/page/form.dart
@@ -1,4 +1,5 @@
 import 'package:fltr/main.dart';
+import 'package:fltr/page/to_do_page.dart';
 import 'package:flutter/material.dart';
 
 class MyFormPage extends StatefulWidget {
@@ -45,6 +46,16 @@ class _MyFormPageState extends State<MyFormPage> {
                 Navigator.pushReplacement(
                   context,
                   MaterialPageRoute(builder: (context) => const MyFormPage()),
+                );
+              },
+            ),
+            ListTile(
+              title: const Text('To Do'),
+              onTap: () {
+                // Route menu ke halaman to do
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ToDoPage()),
                 );
               },
             ),
